ef6a02aa07e840040bb3126ce09e298b870806c9
RTMoo <toktarkazyramazan@gmail.com>
2025-02-25 22:52:56 +0500

    [feat] Сериализаторы для приложения products

---
+++ backend/products/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .product import ProductSerializer
+from .category import CategorySerializer
+
+__all__ = [ProductSerializer, CategorySerializer]
---
+++ backend/products/serializers/product.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from products.models import Product
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "user", "title", "description", "category", "created_at"]
+        read_only_fields = ["id", "user", "created_at"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        validated_data["user"] = user
+        return super().create(validated_data)
