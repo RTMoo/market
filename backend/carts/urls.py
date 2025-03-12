0f8dbc620caa0a8a925c4ba63e4991c97a822c2b
RTMoo <toktarkazyramazan@gmail.com>
2025-03-12 22:37:19 +0500

    [feat-back] Эндпоинт для полной очистки корзины пользователя

--- backend/carts/urls.py
+++ backend/carts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from carts.views import CartAddView, CartView, CartUpdateView, CartDeleteView
+from carts.views import CartAddView, CartView, CartUpdateView, CartDeleteView, CartClearView
 
 urlpatterns = [
     path("", CartView.as_view(), name="cart_list"),
     path("add/", CartAddView.as_view(), name="cart_add"),
     path("update/<int:cart_item_id>/", CartUpdateView.as_view(), name="cart_update"),
     path("delete/<int:cart_item_id>/", CartDeleteView.as_view(), name="cart_delete"),
+    path("clear/", CartClearView.as_view(), name="cart_clear"),
 ]
