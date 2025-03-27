e3f0af9d2a5305b846235a24bd3ee52487c5983a
RTMoo <toktarkazyramazan@gmail.com>
2025-03-27 17:21:37 +0500

    [feat-backend] Модель и сериализатор для Review

---
+++ backend/reviews/models/review.py
@@ -0,0 +1,25 @@
+from django.db.models import (
+    Model,
+    CharField,
+    ForeignKey,
+    CASCADE,
+    DateTimeField,
+    IntegerField,
+)
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class Review(Model):
+    product = ForeignKey(
+        to="products.Product", on_delete=CASCADE, related_name="product_reviews"
+    )
+    buyer = ForeignKey(
+        to="accounts.CustomUser", on_delete=CASCADE, related_name="buyer_reviews"
+    )
+    rating = IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = CharField(max_length=255, blank=True, null=True)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
