1d1da44c8123471ea5d5693e74a20749095a3ad2
RTMoo <toktarkazyramazan@gmail.com>
2025-04-01 09:45:57 +0500

    [feat-backend] Список продуктов с пагинацией и просмотр конкретного продукта #6

--- backend/products/views/product_view.py
+++ backend/products/views/product_view.py
@@ -1,19 +1,33 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import status
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
+from commons.paginations import ProductPagination
 
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_product_list(request):
-    pass
+    product_list = Product.objects.all()
+    paginator = ProductPagination()
+    queryset = paginator.paginate_queryset(queryset=product_list, request=request)
+    data = ProductSerializer(instance=queryset, many=True).data
+
+    return paginator.get_paginated_response(data)
 
 
 @api_view(["GET"])
 @permission_classes([AllowAny])
 def get_product_detail(request, product_id):
-    pass
+    product = Product.objects.filter(id=product_id).first()
 
+    if not product:
+        return Response({"Not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    data = ProductSerializer(instance=product).data
+    return Response(data=data, status=status.HTTP_200_OK)
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
