d493e8c81abc6c24175dbcaa83015a83346fe802
RTMoo <toktarkazyramazan@gmail.com>
2025-04-05 21:09:35 +0500

    [fix-backend] Добавлен поле role в сериализатор

--- backend/profiles/serializers/profile.py
+++ backend/profiles/serializers/profile.py
@@ -1,13 +1,14 @@
-from rest_framework.serializers import ModelSerializer, EmailField
+from rest_framework.serializers import ModelSerializer, EmailField, CharField
 from profiles.models import Profile
 from phonenumber_field.modelfields import PhoneNumberField
 
 
 class ProfileSerializer(ModelSerializer):
     email = EmailField(source="user.email")
+    role = CharField(source="user.role")
     phone_number = PhoneNumberField()
 
     class Meta:
         model = Profile
-        fields = ["first_name", "last_name", "phone_number", "email"]
-        read_only_fields = ["email"]
+        fields = ["first_name", "last_name", "phone_number", "email", "role"]
+        read_only_fields = ["email", "role"]
