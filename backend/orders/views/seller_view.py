c899b78a4f2d5ce3d7b0f591827c883155ca26fb
RTMoo <toktarkazyramazan@gmail.com>
2025-04-06 23:03:39 +0500

    Добавлены кэшы для orders Fixes #10

--- backend/orders/views/seller_view.py
+++ backend/orders/views/seller_view.py
@@ -4,19 +4,26 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from orders.models import OrderItem, Order
 from orders.serializers import OrderSerializer, OrderItemSerializer
+from django.core.cache import cache
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def seller_order_list(request):
     seller_id = request.user.id
-    orders = (
-        Order.objects.filter(items__seller=seller_id)
-        .distinct()
-        .prefetch_related("items__product")
-    )
+    cache_key = f"seller_{seller_id}_order_list"
+    data = cache.get(cache_key)
 
-    data = OrderSerializer(instance=orders, many=True).data
+    if not data:
+        orders = (
+            Order.objects.filter(items__seller=seller_id)
+            .distinct()
+            .prefetch_related("items__product")
+        )
+
+        data = OrderSerializer(instance=orders, many=True).data
+        cache.set(cache_key, data, timeout=60 * 60)
+
     return Response(data=data, status=status.HTTP_200_OK)
 
 
@@ -24,27 +31,34 @@ def seller_order_list(request):
 @permission_classes([IsAuthenticated])
 def seller_order_detail(request, order_id):
     seller_id = request.user.id
-    order = (
-        Order.objects.filter(id=order_id).prefetch_related("items__product").first()
-    )
-    if not order:
-        return Response(
-            {"detail": "Order not found"}, status=status.HTTP_404_NOT_FOUND
+    cache_key = f"seller_{seller_id}_order_detail_{order_id}"
+
+    order_data = cache.get(cache_key)
+
+    if not order_data:
+        order = (
+            Order.objects.filter(id=order_id).prefetch_related("items__product").first()
         )
+        if not order:
+            return Response(
+                {"detail": "Order not found"}, status=status.HTTP_404_NOT_FOUND
+            )
 
-    # Фильтруем товары, оставляя только те, где seller_id = текущий продавец
-    filtered_items = OrderItem.objects.filter(
-        seller_id=seller_id, order=order
-    ).select_related("product")
+        # Фильтруем товары, оставляя только те, где seller_id = текущий продавец
+        filtered_items = OrderItem.objects.filter(
+            seller_id=seller_id, order=order
+        ).select_related("product")
 
-    order_data = OrderSerializer(order).data
-    order_data["items"] = OrderItemSerializer(filtered_items, many=True).data
+        order_data = OrderSerializer(order).data
+        order_data["items"] = OrderItemSerializer(filtered_items, many=True).data
 
-    # Вычисляем общую стоимость
-    total_price = sum(
-        i["quantity"] * i["product_price"] for i in order_data["items"]
-    )
-    order_data["total_price"] = total_price
+        # Вычисляем общую стоимость
+        total_price = sum(
+            i["quantity"] * i["product_price"] for i in order_data["items"]
+        )
+        order_data["total_price"] = total_price
+
+        cache.set(cache_key, order_data, timeout=60 * 60)
 
     return Response(data=order_data, status=status.HTTP_200_OK)
 
@@ -52,8 +66,9 @@ def seller_order_detail(request, order_id):
 @api_view(["PATCH"])
 @permission_classes([IsAuthenticated])
 def seller_order_status_update(request, order_item_id):
+    order_item = OrderItem.objects.filter(id=order_item_id).select_related("order").first()
+    buyer_id = order_item.order.buyer_id
     seller_id = request.user.id
-    order_item = OrderItem.objects.filter(id=order_item_id).first()
 
     if not order_item:
         return Response(
@@ -78,6 +93,13 @@ def seller_order_status_update(request, order_item_id):
     if item_status in (OrderItem.Status.CANCELED, OrderItem.Status.SHIPPED):
         order_item.status = item_status
         order_item.save()
+        
+        cache_key_buyer = f"buyer_{buyer_id}_order_detail_{order_item.order_id}"
+        cache_key_seller = f"seller_{seller_id}_order_detail_{order_item.order_id}"
+
+        cache.delete(cache_key_buyer)
+        cache.delete(cache_key_seller)
+
         return Response({"detail": "Статус обновлен"}, status=status.HTTP_200_OK)
 
     return Response(
