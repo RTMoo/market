e656f31216e5c9c8c30077aa1315d7c0c6f23433
RTMoo <toktarkazyramazan@gmail.com>
2025-04-06 18:50:41 +0500

    [feat-backend] кэширование приложения profiles Fixes #18

--- backend/profiles/views/profile_view.py
+++ backend/profiles/views/profile_view.py
@@ -4,14 +4,20 @@ from rest_framework import status
 from profiles.serializers import ProfileSerializer
 from profiles.models import Profile
 from rest_framework.decorators import api_view, permission_classes
+from django.core.cache import cache
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_profile_detail(request):
     user_id = request.user.id
-    profile = Profile.objects.filter(user_id=user_id).select_related("user").first()
-    data = ProfileSerializer(instance=profile).data
+    cache_key = f"profile_user_{user_id}"
+
+    data = cache.get(cache_key)
+    if not data:
+        profile = Profile.objects.filter(user_id=user_id).select_related("user").first()
+        data = ProfileSerializer(instance=profile).data
+        cache.set(cache_key, data, 60 * 60)
 
     return Response(data=data, status=status.HTTP_200_OK)
 
@@ -22,11 +28,13 @@ def update_profile(request):
     serializer = ProfileSerializer(data=request.data, partial=True)
     if serializer.is_valid():
         user_id = request.user.id
+        cache_key = f"profile_user_{user_id}"
 
         Profile.objects.filter(user_id=user_id).update(**serializer.validated_data)
         profile = Profile.objects.filter(user_id=user_id).select_related("user").first()
         data = ProfileSerializer(instance=profile).data
 
+        cache.delete(cache_key)
         return Response(data=data, status=status.HTTP_200_OK)
 
     return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
