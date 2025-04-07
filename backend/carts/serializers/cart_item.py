c2959e01a3d29ab5b10557c87506f2dc913b36aa
RTMoo <toktarkazyramazan@gmail.com>
2025-04-07 09:42:55 +0500

    [fix-backend] Убрал ненужный метод в сериализаторе

--- backend/carts/serializers/cart_item.py
+++ backend/carts/serializers/cart_item.py
@@ -38,33 +38,3 @@ class CartItemSerializer(ModelSerializer):
             "total_item_price",
         ]
         read_only_fields = ["id", "total_item_price", "seller"]
-
-    def create(self, validated_data):
-        user = self.context["request"].user.id
-        product_id = validated_data.pop("product_id")
-
-        cart = Cart.objects.filter(user=user).first()
-        product = Product.objects.filter(id=product_id).select_related("seller").first()
-
-        if product.seller_id == user:
-            detail = "Нельзя добавить свои товары в корзину"
-            raise PermissionDenied({"detail": detail})
-
-        if not cart:
-            detail = "Корзина не найдено, возможно вы не авторизованы"
-            raise ValidationError({"detail": detail})
-
-        if not product:
-            detail = "Продукт не найден"
-            raise ValidationError({"detail": detail})
-
-        is_exists = CartItem.objects.filter(cart=cart, product=product).exists()
-
-        if is_exists:
-            detail = "У вас уже есть такой товар в корзине"
-            raise ValidationError({"detail": detail})
-
-        # Изначально только 1 товар в корзине
-        return CartItem.objects.create(
-            cart=cart, product=product, seller=product.seller, quantity=1
-        )
