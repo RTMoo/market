2c395aed1a1249883651e218120edee5fd9f743b
RTMoo <toktarkazyramazan@gmail.com>
2025-04-14 18:31:56 +0500

    [fix-frontend] Исправление апи маршрута

--- frontend/src/api/profile.js
+++ frontend/src/api/profile.js
@@ -1,4 +1,4 @@
 import { request } from "./request";
 
 export const getProfile = () => request("get", "profiles/me/")
-export const updateProfile = (data) => request("patch", "profiles/me/", data)
+export const updateProfile = (data) => request("patch", "profiles/update/", data)
