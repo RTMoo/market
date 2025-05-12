5efecd4951a4724c6d1c92bc1a8739f925eec5c4
RTMoo <toktarkazyramazan@gmail.com>
2025-05-12 11:36:54 +0500

    feat: nginx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Обязательно для корректной работы маршрутов в Nginx
+  build: {
+    outDir: 'dist', // Стандартная папка, совпадает с Dockerfile
+    emptyOutDir: true
+  }
 })
